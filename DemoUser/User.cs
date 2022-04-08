15e7b5f017ddb36e322f337105de1f1842647cd1
msx <552364557@qq.com>
2022-04-08 17:26:22 +0800

    添加项目文件。

---
+++ DemoUser/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUser
+{
+    public class User
+    {
+        private int A, B, X;
+        public User(int a, int b, int x)
+        {
+            A = a;
+            B = b;
+            X = x;
+            if (A > 1 && B == 0)
+            {
+                X = X / A;
+            }
+            else
+            {
+                if (A == 2 || X > 1)
+                {
+                    X = X + 1;
+                }
+            }
+        }
+    }
+
+}
---
+++ UnitTestProject1/UserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoUser;
+
+namespace UserTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 0;
+            int b = 0;
+            int x = 0;
+            User z = new User(a, b, x);
+            Assert.IsTrue(z != null);
+        }
+    }
+
+
+}
